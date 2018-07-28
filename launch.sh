dcca1b84619e8582c273acfd41498097ff93ca1b
jlanej <jlanej@gmail.com>
2018-07-28 16:55:19 -0500

    add cat b

--- launch.sh
+++ launch.sh
@@ -8,6 +8,8 @@ env GOOS=linux GOARCH=amd64 go build -o linux.catbook /Users/Kitty/Go/src/github
 go build -o test.catbook /Users/Kitty/Go/src/github.com/fgeller/bilder/*.go
 
 rsync -avz ./*  arew:/home/freyabison/sland.rotblauer.com/
+rsync -avz /Volumes/Data/iceland/sharePic/*.jpg arew:/home/freyabison/sland.rotblauer.com/img/iceland/
+rsync -avz /Volumes/Data/boots_bandit/*.jpg arew:/home/freyabison/sland.rotblauer.com/img/cats/
 
 
 	ssh freya <<\EOI
